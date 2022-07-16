4ba7a4a7ffde0896772ff38d915b78d1d5d6543f
knanao <nao.7ken@gmail.com>
2022-07-16 21:09:18 +0900

    Add the abc34_c of atcoder

---
+++ atcoder/abc34_c.rs
@@ -0,0 +1,206 @@
+use std::cmp::{Ord, Ordering};
+#[allow(unused_imports)]
+use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::io;
+use std::io::{Read, Write};
+use std::str::FromStr;
+
+struct Scanner<R: Read> {
+    reader: R,
+}
+
+#[allow(dead_code)]
+impl<R: Read> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner { reader: reader }
+    }
+
+    fn read<T: FromStr>(&mut self) -> Option<T> {
+        let token = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|c| c.unwrap() as char)
+            .skip_while(|c| c.is_whitespace())
+            .take_while(|c| !c.is_whitespace())
+            .collect::<String>();
+        if token.is_empty() {
+            None
+        } else {
+            token.parse::<T>().ok()
+        }
+    }
+
+    fn next<T: FromStr>(&mut self) -> T {
+        if let Some(s) = self.read() {
+            s
+        } else {
+            writeln!(io::stderr(), "Terminated with EOF").unwrap();
+            std::process::exit(0);
+        }
+    }
+
+    fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.next()).collect()
+    }
+
+    fn chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
+    }
+
+    fn char(&mut self) -> char {
+        self.chars()[0]
+    }
+}
+
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+}
+
+struct DisjointSet {
+    numtree: usize,
+    data: Vec<usize>,
+    size: Vec<usize>,
+}
+
+#[allow(dead_code)]
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        Self {
+            numtree: n,
+            data: (0..n).map(|x| x).collect::<Vec<usize>>(),
+            size: vec![1; n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.data[x] == x {
+            x
+        } else {
+            let px = self.data[x];
+            self.data[x] = self.find(px);
+            self.data[x]
+        }
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let mut px = self.find(x);
+        let mut py = self.find(y);
+        if px == py {
+            return;
+        }
+        if self.size[py] < self.size[px] {
+            std::mem::swap(&mut px, &mut py);
+        }
+        self.data[px] = py;
+        self.size[py] += self.size[px];
+        self.numtree -= 1;
+    }
+
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        let px = self.find(x);
+        let py = self.find(y);
+        self.data[px] == self.data[py]
+    }
+
+    fn tree_size(&mut self, x: usize) -> usize {
+        let px = self.find(self.data[x]);
+        self.size[px]
+    }
+}
+
+#[allow(dead_code)]
+const INF: usize = 1 << 60;
+
+const MOD: u64 = 1000000007;
+
+fn main() {
+    let cin = io::stdin();
+    let mut sc = Scanner::new(cin.lock());
+
+    let (w, h): (u64, u64) = (sc.next(), sc.next());
+
+    let mut a = w + h - 2;
+    let mut a_per = 1;
+    while a > 1 {
+        a_per = a_per * a % MOD;
+        a -= 1;
+    }
+
+    let mut b = h - 1;
+    let mut b_per = 1;
+    while b > 1 {
+        b_per = b_per * b % MOD;
+        b -= 1;
+    }
+
+    let mut c = w - 1;
+    let mut c_per = 1;
+    while c > 1 {
+        c_per = c_per * c % MOD;
+        c -= 1;
+    }
+
+    let mut d = b_per * c_per % MOD;
+    d = modinv(d, MOD);
+    println!("{}", a_per * d % MOD)
+}
+
+fn modinv(x: u64, m: u64) -> u64 {
+    let mut a = x;
+    let mut b = m;
+    let mut u: i32 = 1;
+    let mut v: i32 = 0;
+
+    while b > 0 {
+        let t = a / b;
+        a -= t * b;
+        std::mem::swap(&mut a, &mut b);
+        u -= t as i32 * v;
+        std::mem::swap(&mut u, &mut v);
+    }
+    u %= m as i32;
+    if u < 0 {
+        u += m as i32;
+    }
+    return u as u64;
+}
